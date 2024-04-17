064c6a4977c38d62915cd3dc474c82a79f740417
Havard Lovas <havard.s.lovas@ntnu.no>
2024-04-17 10:59:20 +0200

    Test instantiation with from_ methods

--- rad4sea/atmos.py
+++ rad4sea/atmos.py
@@ -1,4 +1,46 @@
 # A module for simple manipulation of radiometric data 
 
-class TransformRad():
-    
+class OceanRad():
+    """
+    A class representing ocean radiometry data.
+
+    Can be initialized from fish or bird parameters.
+    """
+    def __init__(self, data_source, parameters):
+        """
+        Initializes the OceanRad object.
+
+        Args:
+            data_source (str): Source of the data ("fish" or "bird").
+            parameters (dict): Parameters for the data source.
+        """
+
+        self.data_source = data_source
+        self.parameters = parameters
+    @classmethod
+    def from_fish(cls, fish_parameters):
+        """
+        Creates an OceanRad object from fish parameters.
+
+        Args:
+            fish_parameters (dict): Parameters specific to fish data.
+
+        Returns:
+            OceanRad: An OceanRad object initialized with fish data.
+        """
+
+        return cls(data_source="fish", parameters=fish_parameters)
+
+    @classmethod
+    def from_bird(cls, bird_parameters):
+        """
+        Creates an OceanRad object from bird parameters.
+
+        Args:
+            bird_parameters (dict): Parameters specific to bird data.
+
+        Returns:
+            OceanRad: An OceanRad object initialized with bird data.
+        """
+
+        return cls(data_source="bird", parameters=bird_parameters)
